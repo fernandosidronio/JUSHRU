30219373b999d3ad37fc99f4ab157d0b46c43d24
SIDRONIUS <64292033+fernandosidronio@users.noreply.github.com>
2024-07-16 22:37:52 +0000

    acrescentando outra frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Chegando em casa eu bebo um copo d'agua</h1>
+    return <h1>Chegando em casa eu bebo um copo d'agua. Na verdade é melhor eu perder a inibição.</h1>
 }
 
 export default Home;
